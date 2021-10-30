edb92a75c74960e73d03632c4478664b330e14da
Marcin Czenko <marcin@everydayproductive.com>
2021-10-30 04:48:05 +0200

    Updates component-navigator-react to also enjoy sorting with sortIndex

--- workspaces/demo-component-navigator/src/navigationData.js
+++ workspaces/demo-component-navigator/src/navigationData.js
@@ -1,14 +1,13 @@
 const navigationData = {
   site: {
     siteMetadata: {
-      title: 'Netscape Navigator'
+      title: 'Component Navigator'
     }
   },
   config: {
     nodes: [
       { title: 'Nav Group 1', tag: 'tag1' },
-      { title: 'Nav Group 2', tag: 'tag2' },
-      { title: 'Nav Group 3', tag: 'tag3' }
+      { title: 'Nav Group 2', tag: 'tag2' }
     ]
   },
   navigation: {
@@ -18,7 +17,8 @@ const navigationData = {
           frontmatter: {
             title: 'Group 1',
             path: '/',
-            tag: 'tag1'
+            tag: 'tag1',
+            sortIndex: 1
           },
           headings: [
             { value: 'Heading 1', path: '/heading1' },
@@ -31,7 +31,8 @@ const navigationData = {
           frontmatter: {
             title: 'Group 2',
             path: '/group-2',
-            tag: 'tag2'
+            tag: 'tag1',
+            sortIndex: 2
           }
         }
       },
@@ -40,7 +41,7 @@ const navigationData = {
           frontmatter: {
             title: 'Group 3',
             path: '/group-3',
-            tag: 'tag3'
+            tag: 'tag2'
           },
           headings: [
             { value: 'Heading 1', path: '/group-3/heading1' },
