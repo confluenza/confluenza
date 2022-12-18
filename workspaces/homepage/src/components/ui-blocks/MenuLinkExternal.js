281680b339ada0e0f209bd0fbb3c78099b36e366
Marcin Czenko <marcin.czenko@pm.me>
2022-12-18 01:21:05 +0100

    Updates positioning in the header for small screens

--- workspaces/homepage/src/components/ui-blocks/MenuLinkExternal.js
+++ workspaces/homepage/src/components/ui-blocks/MenuLinkExternal.js
@@ -1,17 +1,18 @@
 import styled from '@emotion/styled'
 
-const MenuLinkExternal = styled.a({
-  display: 'inline-block',
-  color: '#D20DE7',
-  fontFamily: 'Roboto Mono, monospace',
-  margin: '5px 20px',
-  fontSize: '10pt',
-  '&:hover': {
-    color: 'white',
-    textDecoration: 'none'
-  }
-}, ({ styles }) => (
-  { ...styles }
-))
+const MenuLinkExternal = styled.a(
+  {
+    display: 'inline-block',
+    color: '#D20DE7',
+    fontFamily: 'Roboto Mono, monospace',
+    margin: '5px 10px',
+    fontSize: '10pt',
+    '&:hover': {
+      color: 'white',
+      textDecoration: 'none'
+    }
+  },
+  ({ styles }) => ({ ...styles })
+)
 
 export { MenuLinkExternal }
